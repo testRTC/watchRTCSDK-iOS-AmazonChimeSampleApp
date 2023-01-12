336e3625b39ff9aae4fa2ebd69b72cd27d123f36
Yulian Baranetskyy <yulian.baranetskyy@gmail.com>
2023-01-12 12:46:45 +0200

    Initial commit (added clean Chime demo app)

---
+++ AmazonChimeSDKDemo/AppConfiguration.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfiguration.swift
+//  AmazonChimeSDKDemo
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+struct AppConfiguration {
+    static let url = "https://uqrh3xv397.execute-api.us-east-1.amazonaws.com/Prod/"
+    static let region = "us-east-1"
+
+    static let broadcastBundleId = "NO_NEED_TO_CHANGE"
+    static let appGroupId = "NO_NEED_TO_CHANGE"
+}
